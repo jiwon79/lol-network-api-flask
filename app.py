ea8486a2362e747b0ecb702a9c25e1e3ddf194f0
jiwon79 <19-079@ksa.hs.kr>
2021-08-04 18:08:24 +0900

    feat: Add CORS setting

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 from flask import Flask
+from flask_cors import CORS
+
+from modules.opgg import *
+
+ORIGIN = [
+    'http://lol-network.netlify.app/',
+    'https://lol-network.netlify.app/',
+    'http://lol-network.netlify.app',
+    'https://lol-network.netlify.app',
+    'http://localhost',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:5500',
+    'http://localhost:5501',
+    'http://localhost:8000',
+    'http://127.0.0.1',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:3001',
+    'http://127.0.0.1:3002',
+    'http://127.0.0.1:5500',
+    'http://127.0.0.1:5501',
+    'http://127.0.0.1:8000',
+]
+
 app = Flask(__name__)
+cors = CORS(app, resources={
+  r"*": {"origin": ORIGIN}
+})
 
 @app.route("/")
-def hello():
-    return "Hello, World!!!!!!"
+def main():
+  result = {"result": '결과'}
+  return result
+
+# if __name__ == "__main__":
+#   app.run(debug=True)
